d9d2bd3d58e20b95fb81a6eec4d7c5772a547ade
Your Name <you@example.com>
2024-04-18 15:30:35 -0700

    Push my code to github

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Please enter your sentence to count your word: "
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+//Print the arrayes of the word
+console.log(words);
+//Print the word count of the sentence  to the sonsole
+console.log(`Your sentence word count is ${words.length}`);
